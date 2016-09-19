1a6af02eb4500f2b74d23d8e3452ff9baf858861
yveg <gambaly@gmail.com>
2016-09-19 12:12:58 +0200

    Collection, HashSet, ArrayList debut 2.0

--- src/test1/classes/Film.java
+++ src/test1/classes/Film.java
@@ -10,5 +10,55 @@ package test1.classes;
  * @author admin
  */
 public class Film {
+    //private String titre;
+    public static String titre;
+    private Integer duree;
+    private String synopsis;
+    private Integer annee;
+
+    /*@Override
+    public String toString() {
+        return "film: "+titre+" " +duree+" " +synopsis+" " +annee; //To change body of generated methods, choose Tools | Templates.
+    }*/
+    @Override 
+     public String toString() {
+        return "film: "+titre+" " +duree+" " +synopsis+" "; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Integer getDuree() {
+        return duree;
+    }
+
+    public void setDuree(Integer duree) {
+        this.duree = duree;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Integer getAnnee() {
+        return annee;
+    }
+
+    public void setAnnee(Integer annee) {
+        this.annee = annee;
+    }
+
+    
+
+   
     
 }
